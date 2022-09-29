5b9ca8df6c15d85ec3428164f74f3254b90c5266
khalidsharker <76488602+khalidsharker@users.noreply.github.com>
2022-09-30 00:34:01 +0600

    update 0.2 responsive

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const header =document.querySelector('Header');
+const header =document.querySelector('header');
 
 window.addEventListener('scroll', function() {
     header.classList.toggle ("sticky", window.scrollY > 0);
 })
 
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar'); 
+let headerBg = document.querySelector('header');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    headerBg.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+    header.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+    distance: '25px',
+    duration: 2500,
+    reset: true
+});
+
+sr.reveal('.home-text',{delay:190, origin:'bottom'});
+sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'});
