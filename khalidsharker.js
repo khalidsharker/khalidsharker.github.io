77a199e0dffe1633f2fd7ee77562f010e8e390d5
khalidsharker <76488602+khalidsharker@users.noreply.github.com>
2022-09-29 15:29:17 +0600

    update v 0.2

--- khalidsharker.js
+++
@@ -1,4 +0,0 @@
-let num1 = 5;
-let num2 = 6;
-
-console.log(num1+ num2);
---
+++ script.js
@@ -0,0 +1,6 @@
+const header =document.querySelector('Header');
+
+window.addEventListener('scroll', function() {
+    header.classList.toggle ("sticky", window.scrollY > 0);
+})
+
